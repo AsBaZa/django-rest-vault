8ff281e5150126b7062454b611ea6a5b7bab4947
AsBaZa <asier.badiola@outlook.es>
2021-02-13 18:57:05 +0100

    STATIC_ROOT config

--- django_rest_vault/settings.py
+++ django_rest_vault/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # https://devcenter.heroku.com/articles/django-assets
 
 STATIC_URL = '/static/'
 
